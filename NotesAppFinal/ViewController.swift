1b9cf9941b68f3bd9dbb3ed11ecfcc21b40775e9
navimalhotra <navimalhotra124@gmail.com>
2020-01-28 17:20:28 -0500

    Load Data.

--- NotesAppFinal/ViewController.swift
+++ NotesAppFinal/ViewController.swift
@@ -7,12 +7,43 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate, UINavigationControllerDelegate, UIImagePickerControllerDelegate, UITextViewDelegate {
 
+    @IBOutlet weak var noteInfoView: UIView!
+    @IBOutlet weak var noteImageViewView: UIView!
+    
+    @IBOutlet weak var noteNameLabel: UITextField!
+    
+    @IBOutlet weak var noteImageView: UIImageView!
+    @IBOutlet weak var noteDescriptionLabel: UITextView!
+    
+    var managedObjectContext: NSManagedObjectContext?{
+        return (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    }
+    
+    var notesFetchedResultsController: NSFetchedResultsController<Note>!
+    var notes = [Note]()
+    var note: Note?
+    var isExisting = false
+    var indexpath: Int?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //Load Data
+        if let note = note {
+            noteNameLabel.text = note.noteName
+            noteDescriptionLabel.text = note.noteDescription
+            noteImageView.image = UIImage(data: note.noteImage! as Data)
+        }
+        
+        if noteNameLabel.text != "" {
+            isEditing = true
+        }
+        
     }
 
 
